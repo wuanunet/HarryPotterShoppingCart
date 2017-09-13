69a844463b39a9434b33d941c3bca394392c7b91
Cheng-Ju Wu <wuanunet@hotmail.com>
2017-09-13 20:18:23 +0800

    新增第二個測試案例：購買哈利波特第一、二集各一本，享有 5% 折扣，計算購物車費用應為190元，但結果為200元。(第一綠燈，第二紅燈)

--- HarryPotterShoppingCart.Tests/ShoppingCartServiceTests.cs
+++ HarryPotterShoppingCart.Tests/ShoppingCartServiceTests.cs
@@ -35,5 +35,28 @@ namespace HarryPotterShoppingCart.Tests
             //// Assert
             actual.Should().Be(expected);
         }
+
+        [Fact]
+        public void CalculateTest_購買HerryPotter第一_二集各一本_價格應該是100_乘_2_乘折扣_0_95_所以購物車費用應為_190_元()
+        {
+            //// Arrange
+            var expected = 190;
+            var shoppingCart = new ShoppingCartEntity
+            {
+                Products = new List<ProductEntity>
+                {
+                    new ProductEntity { Qty = 1, Name = "HerryPotter1", Price = 100 },
+                    new ProductEntity { Qty = 1, Name = "HerryPotter2", Price = 100 }
+                }
+            };
+
+            decimal actual = 0m;
+
+            //// Act
+            actual = this._target.Calculate(shoppingCart);
+
+            //// Assert
+            actual.Should().Be(expected);
+        }
     }
 }
