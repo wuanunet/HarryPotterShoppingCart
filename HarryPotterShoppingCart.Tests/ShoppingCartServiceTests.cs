eaa3b4e483b8993b02bcf4813a43cdb84b20dedf
Cheng-Ju Wu <wuanunet@hotmail.com>
2017-09-14 11:46:42 +0800

    重構測試案例：建立 TestDataSource Class 與測試案例套用 MemberData。

--- HarryPotterShoppingCart.Tests/ShoppingCartServiceTests.cs
+++ HarryPotterShoppingCart.Tests/ShoppingCartServiceTests.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
-using FluentAssertions;
+﻿using FluentAssertions;
 using HarryPotterShoppingCart.Models;
 using Xunit;
 
 namespace HarryPotterShoppingCart.Tests
 {
+
+
     public class ShoppingCartServiceTests
     {
         private readonly ShoppingCartService _target;
@@ -14,42 +15,11 @@ namespace HarryPotterShoppingCart.Tests
             this._target = new ShoppingCartService();
         }
 
-        [Fact]
-        public void CalculateTest_購買HerryPotter第一集一本_沒有折扣_購物車費用應為_100_元()
+        [Theory]
+        [MemberData(nameof(TestDataSource.TestData), MemberType = typeof(TestDataSource))]
+        public void CalculateTest_購買HerryPotter書籍優惠價格計算(ShoppingCartEntity shoppingCart, decimal expected)
         {
             //// Arrange
-            var expected = 100;
-            var shoppingCart = new ShoppingCartEntity
-            {
-                Products = new List<ProductEntity>
-                {
-                    new ProductEntity { Qty = 1, Name = "HerryPotter1", Price = 100 }
-                }
-            };
-
-            decimal actual = 0m;
-
-            //// Act
-            actual = this._target.Calculate(shoppingCart);
-
-            //// Assert
-            actual.Should().Be(expected);
-        }
-
-        [Fact]
-        public void CalculateTest_購買HerryPotter第一_二集各一本_價格應該是100_乘_2_乘折扣_0_95_所以購物車費用應為_190_元()
-        {
-            //// Arrange
-            var expected = 190;
-            var shoppingCart = new ShoppingCartEntity
-            {
-                Products = new List<ProductEntity>
-                {
-                    new ProductEntity { Qty = 1, Name = "HerryPotter1", Price = 100 },
-                    new ProductEntity { Qty = 1, Name = "HerryPotter2", Price = 100 }
-                }
-            };
-
             decimal actual = 0m;
 
             //// Act
---
+++ HarryPotterShoppingCart.Tests/TestDataSource.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using HarryPotterShoppingCart.Models;
+
+namespace HarryPotterShoppingCart.Tests
+{
+    public class TestDataSource
+    {
+        public static IEnumerable<object[]> TestData => _data;
+
+        private static readonly List<object[]> _data = new List<object[]>
+        {
+            //// 第一個測試案例：購買哈利波特第一集一本，有沒折扣，購物車金額應為 100 元
+            new object[]
+            {
+                new ShoppingCartEntity
+                {
+                    Products = new List<ProductEntity>
+                    {
+                        new ProductEntity {Qty = 1, Name = "HerryPotter1", Price = 100}
+                    }
+                }, 100M
+            },
+            //// 第二個測試案例：購買哈利波特第一、二集各一本，享有折扣 5%，購物車金額應為 190 元
+            new object[]
+            {
+                new ShoppingCartEntity
+                {
+                    Products = new List<ProductEntity>
+                    {
+                        new ProductEntity {Qty = 1, Name = "HerryPotter1", Price = 100},
+                        new ProductEntity {Qty = 1, Name = "HerryPotter2", Price = 100}
+                    }
+                }, 190M
+            }
+
+        };
+    }
+}
